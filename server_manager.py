1e87f6d3eda08fdee4a3030609f093f97f17d8c6
Kirill <kirill.shakhov14@gmail.com>
2020-09-08 21:52:51 +0300

    add help

--- server_manager.py
+++ server_manager.py
@@ -1,4 +1,5 @@
 from server import Server
 
+#ServerOrthodoxLamp = Server('KEY', GROUP_ID, "NAME")
 ServerOrthodoxLamp = Server('7349d6b7788144b7577e465bd5dc919ea6a1f580349af1a4309717f84074ea5432382894d5da8d4763531', 185976227, "Orthodox_lamp2")
 ServerOrthodoxLamp.start()
